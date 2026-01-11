e9525d45cbc0b1d0b168cc46514441d4864c6e97
karim-chehab-darwinz <kchehab@darwinz.ai>
2026-01-11 10:54:30 +0200

    - Added Google Tag Manager script for all pages

--- app/[locale]/layout.tsx
+++ app/[locale]/layout.tsx
@@ -70,9 +70,29 @@ export default async function RootLayout({
   }
   return (
     <html lang={locale} dir={locale === "ar" ? "rtl" : "ltr"} suppressHydrationWarning>
+      <head>
+        {/* Google Tag Manager */}
+        <Script id="gtm-script" strategy="beforeInteractive">
+          {`
+            (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+            new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+            j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+            'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+            })(window,document,'script','dataLayer','GTM-TGZB44QG');
+          `}
+        </Script>
+        {/* End Google Tag Manager */}
+      </head>
       <body
         className={`${locale === "ar" ? cairo.className : geistSans.className} antialiased`}
       >
+        {/* Google Tag Manager (noscript) */}
+        <noscript>
+          <iframe src="https://www.googletagmanager.com/ns.html?id=GTM-TGZB44QG"
+            height="0" width="0" style={{ display: "none", visibility: "hidden" }}></iframe>
+        </noscript>
+        {/* End Google Tag Manager (noscript) */}
+
         <NextIntlClientProvider>
           <ReactQueryProvider>
             <div className="min-h-dvh h-full flex flex-col justify-between">
