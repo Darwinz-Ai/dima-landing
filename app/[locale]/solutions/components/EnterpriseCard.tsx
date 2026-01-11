c226d73ff4861492dd02a9d0b5c0816a4cd0467a
karim-chehab-darwinz <kchehab@darwinz.ai>
2026-01-11 10:59:57 +0200

    - Fixed UI Bug in enterprise card in solutions page

--- app/[locale]/solutions/components/EnterpriseCard.tsx
+++ app/[locale]/solutions/components/EnterpriseCard.tsx
@@ -4,7 +4,7 @@ import Image from "next/image";
 
 function EntrepriseCard({ icon, title, description }: CardType) {
     return (
-        <article className="flex flex-col justify-center items-center bg-[#95DDEE]/50 p-8 rounded-xl h-full min-h-80">
+        <article className="flex flex-col justify-center bg-[#95DDEE]/50 p-8 rounded-xl h-full min-h-80">
             <div className="flex-1 flex flex-col gap-8" >
                 {/* Icon */}
                 <div className={`flex lg:flex-col  gap-4`}>
