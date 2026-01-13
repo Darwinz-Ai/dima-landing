026fb492a2c5542b0afffde3132081ec52a0871b
karim-chehab-darwinz <kchehab@darwinz.ai>
2026-01-13 11:19:36 +0200

    - Second attempt to integrate Google Tag Manager

--- app/[locale]/layout.tsx
+++ app/[locale]/layout.tsx
@@ -7,10 +7,10 @@ import { routing } from "@/i18n/routing";
 import { notFound } from "next/navigation";
 import Footer from "@/components/shared/footer/Footer";
 import ReactQueryProvider from "../ReactQueryProvider";
-// import PageLoader from "@/components/shared/PageLoader";
 import { Toaster } from "sonner";
 import Script from "next/script";
 import { buildLocalizedMetadata } from "@/lib/seo";
+import { GoogleTagManager } from '@next/third-parties/google';
 
 const geistSans = Geist({
   subsets: ["latin"],
@@ -70,29 +70,10 @@ export default async function RootLayout({
   }
   return (
     <html lang={locale} dir={locale === "ar" ? "rtl" : "ltr"} suppressHydrationWarning>
-      <head>
-        {/* Google Tag Manager */}
-        <Script id="gtm-script" strategy="beforeInteractive">
-          {`
-            (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
-            new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-            j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-            'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-            })(window,document,'script','dataLayer','GTM-TGZB44QG');
-          `}
-        </Script>
-        {/* End Google Tag Manager */}
-      </head>
+      <GoogleTagManager gtmId="GTM-TGZB44QG" />
       <body
         className={`${locale === "ar" ? cairo.className : geistSans.className} antialiased`}
       >
-        {/* Google Tag Manager (noscript) */}
-        <noscript>
-          <iframe src="https://www.googletagmanager.com/ns.html?id=GTM-TGZB44QG"
-            height="0" width="0" style={{ display: "none", visibility: "hidden" }}></iframe>
-        </noscript>
-        {/* End Google Tag Manager (noscript) */}
-
         <NextIntlClientProvider>
           <ReactQueryProvider>
             <div className="min-h-dvh h-full flex flex-col justify-between">
