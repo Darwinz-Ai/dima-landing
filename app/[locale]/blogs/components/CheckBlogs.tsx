3d35c40ee6bfa5c4ad774b6bff36080d1d32dfdd
karim-chehab-darwinz <kchehab@darwinz.ai>
2026-02-01 13:05:09 +0200

    - Optimized check more blogs section in blog single view page

--- app/[locale]/blogs/components/CheckBlogs.tsx
+++ app/[locale]/blogs/components/CheckBlogs.tsx
@@ -1,42 +1,35 @@
-"use client";
 import React from 'react'
 import GroupedBlogs from './GroupedBlogs'
-import { useTranslations } from 'next-intl'
-import LoadingAnimation from '@/components/shared/LoadingAnimation'
-import { notFound } from 'next/navigation'
-import useBlogs from '../hooks/useBlogs'
-import BlogCardSkeleton from './BlogCardSkeleton';
 import BlogCard from './BlogCard';
+import { fetchBlogs } from '@/lib/firebase/blogsFunctions';
+import { getLocale, getTranslations } from 'next-intl/server';
 
-const CheckBlogs = () => {
-    const t = useTranslations("Blog")
-    const { data: blogs, isLoading: blogsLoading, isError: blogsError } = useBlogs(4);
-    if (blogsLoading) return <LoadingAnimation />
-    if (blogsError) return notFound()
-    return (
-        <div className="container mx-auto">
-            <div className="flex-1">
-                <GroupedBlogs title={t("checkMoreBlogs")} hrefViewAll="/blogs">
-                    {blogsError && <p>Failed to load more blogs</p>}
-                    <ul className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-8 w-full">
-                        {
-                            blogsLoading
-                                ? Array.from({ length: 4 }).map((_, i) => (
-                                    <li key={`skeleton-blogs-${i}`}>
-                                        <BlogCardSkeleton />
-                                    </li>
-                                ))
-                                : blogs?.map((blog) => (
+const CheckBlogs = async () => {
+    const t = await getTranslations("Blog")
+    const locale = await getLocale();
+
+    try {
+        const blogs = await fetchBlogs(locale, [], 4);
+        return (
+            <div className="container mx-auto">
+                <div className="flex-1">
+                    <GroupedBlogs title={t("checkMoreBlogs")} hrefViewAll="/blogs">
+                        <ul className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-8 w-full">
+                            {
+                                blogs?.map((blog) => (
                                     <li key={blog.id}>
                                         <BlogCard blog={blog} />
                                     </li>
                                 ))
-                        }
-                    </ul>
-                </GroupedBlogs>
+                            }
+                        </ul>
+                    </GroupedBlogs>
+                </div>
             </div>
-        </div>
-    )
+        )
+    } catch (error: any) {
+        return <p>Error:{error.message}</p>
+    }
 }
 
 export default CheckBlogs
