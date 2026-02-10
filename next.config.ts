8aab619ec99a4821f0314085f0fa16f9c322e2ee
karim-chehab-darwinz <kchehab@darwinz.ai>
2026-02-10 14:11:49 +0200

    - Readded output standalone for building

--- next.config.ts
+++ next.config.ts
@@ -3,7 +3,7 @@ import createNextIntlPlugin from "next-intl/plugin";
 
 const nextConfig: NextConfig = {
   /* config options here */
-  // output: "standalone",
+  output: "standalone",
   images: {
     remotePatterns: [{
       protocol: "https",
