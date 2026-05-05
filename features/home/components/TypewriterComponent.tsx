8e9e56c9a7027e506bcc0649f7ab8d7114178ee5
karim-chehab-darwinz <kchehab@darwinz.ai>
2026-05-05 14:03:10 +0300

    - Added GSAP and @gsap/react dependencies for animation support - Refactored TypewriterComponent to utilize GSAP for text animations

--- features/home/components/TypewriterComponent.tsx
+++ features/home/components/TypewriterComponent.tsx
@@ -1,66 +1,72 @@
 "use client";
-import { useEffect, useState } from 'react'
-import { useLocale, useTranslations } from 'next-intl';
+import { useEffect, useRef, useState } from 'react'
+import { useTranslations } from 'next-intl';
+import { useGSAP } from "@gsap/react"
+import { TextPlugin } from 'gsap/all';
 
 import RequestDemoButton from '@/components/shared/form/RequestDemoButton';
-import Typewriter from "typewriter-effect";
+
+import gsap from 'gsap';
 
 import { heroSlides } from '@/data/home-page';
 
 const TypewriterComponent = () => {
     const t = useTranslations("Home.hero");
-    const locale = useLocale();
-    const isRTL = locale === "ar";
-
     const slides = heroSlides.map((slide) => t(slide.translationKey));
+    const [isMounted, setIsMounted] = useState<boolean>(false);
 
-    const [mounted, setMounted] = useState<boolean>(false);
+    const container = useRef<HTMLDivElement>(null);
+    const textRef = useRef<HTMLSpanElement>(null);
 
     useEffect(() => {
-        const timer = setTimeout(() => {
-            setMounted(true);
-        }, 1000)
-
-        return () => clearTimeout(timer);
+        setIsMounted(true);
     }, [])
+
+    useGSAP(() => {
+        gsap.registerPlugin(TextPlugin);
+
+        const tl = gsap.timeline({
+            repeat: -1
+        })
+
+        slides.forEach((slide) => {
+            // Write text
+            tl.to(textRef.current, {
+                duration: slide.length * 0.05,
+                text: {
+                    value: slide,
+                    rtl: false
+                },
+                ease: "none"
+            })
+                .to({}, { duration: 2 }) // Pause
+
+                // Delete text
+                .to(textRef.current, {
+                    duration: slide.length * 0.03,
+                    text: {
+                        value: "",
+                        rtl: true,
+                    },
+                    ease: "none"
+                })
+        })
+
+    }, { scope: container, dependencies: [slides] })
+
     return (
-        <div className="flex-1 space-y-6 w-full">
+        <div ref={container} className="flex-1 space-y-6 w-full">
             <h1 className="text-[24px] md:text-[44px] mb-8 min-h-32 md:min-h-[198px] lg:min-h-[264px] xl:min-h-[264px] 2xl:min-h-[198px]">
                 <span>{t("titlePrefix")}</span>{" "}
-
-                {!mounted ? (
-                    <>
-                        <span className='text-primary'>{slides[0]}</span>
-                        <span>|</span>
-                    </>
+                {isMounted ? (
+                    <span
+                        ref={textRef}
+                        className="text-primary"
+                    ></span>
                 ) : (
-                    <Typewriter
-                        options={{
-                            autoStart: true,
-                            loop: true,
-                            delay: isRTL ? 160 : 35,
-                            deleteSpeed: isRTL ? 150 : 25,
-                            wrapperClassName: "text-primary",
-                            stringSplitter: (text: string) => {
-                                if (isRTL) {
-                                    const words = text.split(" ");
-                                    return words.map(word => `${word} `)
-                                }
-                                return text.split("");
-                            },
-                        }}
-                        onInit={(typewriter) => {
-                            slides.forEach((slide) => {
-                                typewriter
-                                    .typeString(slide)
-                                    .pauseFor(3000)
-                                    .deleteAll()
-                                    .start()
-                            })
-                        }}
-                        component={"span"}
-                    />
+                    <span className='text-primary'>{slides[0]}</span>
                 )}
+                <span className='animate-pulse'>|</span>
             </h1>
             <RequestDemoButton size={"xl"} location='Home_hero' />
         </div>
