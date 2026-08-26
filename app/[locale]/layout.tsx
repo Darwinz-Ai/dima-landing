ca30c3eaf4a9fa8635cb5154fc4e880cef2dfc8a
karim-chehab-darwinz <kchehab@darwinz.ai>
2026-08-26 17:18:06 +0300

    feat(script): Added linkedin script tag in header

--- app/[locale]/layout.tsx
+++ app/[locale]/layout.tsx
@@ -120,6 +120,35 @@ export default async function RootLayout({
           src="https://static.claydar.com/init.v1.js?id=cxOAeXXAB5"
           strategy="lazyOnload"
         />
+
+        {/* LinkedIn Insight Tag JS */}
+        <Script id="linkedin-insight-tag" strategy="lazyOnload">
+          {`
+            _linkedin_partner_id = "10781313";
+            window._linkedin_data_partner_ids = window._linkedin_data_partner_ids || [];
+            window._linkedin_data_partner_ids.push(_linkedin_partner_id);
+            (function(l) {
+              if (!l){window.lintrk = function(a,b){window.lintrk.q.push([a,b])};
+              window.lintrk.q=[]}
+              var s = document.getElementsByTagName("script")[0];
+              var b = document.createElement("script");
+              b.type = "text/javascript";b.async = true;
+              b.src = "https://snap.licdn.com/li.lms-analytics/insight.min.js";
+              s.parentNode.insertBefore(b, s);})(window.lintrk);
+          `}
+        </Script>
+
+        {/* LinkedIn Insight Tag NoScript Fallback */}
+        <noscript>
+          {/* eslint-disable-next-line @next/next/no-img-element */}
+          <img
+            height="1"
+            width="1"
+            style={{ display: "none" }}
+            alt=""
+            src="https://px.ads.linkedin.com/collect/?pid=10781313&fmt=gif"
+          />
+        </noscript>
       </body>
     </html>
   );
