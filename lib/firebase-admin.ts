df71777287b8cdd48b4d808a5be2251901e4d845
karim-chehab-darwinz <kchehab@darwinz.ai>
2026-09-10 19:42:38 +0300

    fix build issue with firebase-admin

--- lib/firebase-admin.ts
+++ lib/firebase-admin.ts
@@ -1,14 +1,23 @@
 import { initializeApp, getApps, cert } from "firebase-admin/app";
 import { getFirestore } from "firebase-admin/firestore";
 
+const projectId = process.env.FIREBASE_PROJECT_ID;
+const clientEmail = process.env.FIREBASE_CLIENT_EMAIL;
+const privateKey = process.env.FIREBASE_PRIVATE_KEY;
+
 if (!getApps().length) {
-    initializeApp({
-        credential: cert({
-            projectId: process.env.FIREBASE_PROJECT_ID,
-            clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
-            privateKey: process.env.FIREBASE_PRIVATE_KEY?.replace(/\\n/g, "\n"),
-        }),
-    });
+    if (projectId && clientEmail && privateKey) {
+        initializeApp({
+            credential: cert({
+                projectId,
+                clientEmail,
+                privateKey: privateKey.replace(/\\n/g, "\n"),
+            }),
+        });
+    } else {
+        console.warn("Firebase Admin credentials missing. Skipping initialization during build.");
+    }
 }
 
-export const adminDb = getFirestore();
+// afely export null during build, active instance during Cloud Run runtime
+export const adminDb = getApps().length > 0 ? getFirestore() : null;
